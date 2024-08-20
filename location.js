e968328245d23c8a932c072123a4a259149dae01
grishma <grishmachudal9988@gmail.com>
2024-08-20 08:51:06 +0545

    Grishma's code . 2

---
+++ location.js
@@ -0,0 +1,20 @@
+const charali=document.getElementById('Charali').addEventListener('click' , ()=>{
+    // alert('pika');
+
+});
+const dhaijan=document.getElementById('Charali').addEventListener('click' , ()=>{
+    // alert('pika');
+
+});
+const dhulabari=document.getElementById('Charali').addEventListener('click' , ()=>{
+    // alert('pika');
+
+});
+const ittavatta=document.getElementById('Charali').addEventListener('click' , ()=>{
+    // alert('pika');
+
+});
+const kakarvitta=document.getElementById('Charali').addEventListener('click' , ()=>{
+    // alert('pika');
+
+});
